54fedff35e4f773ff14e5ff3c7ac6bc298a95585
Ceren Turgut <veyselceren25@gmail.com>
2021-02-14 18:56:32 +0300

    14.02

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -30,10 +30,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==15)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 18)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
         }
 
